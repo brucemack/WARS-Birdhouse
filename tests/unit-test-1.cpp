d04f3810286bd334b6ebeb9cf04c8974daa9da89
Bruce MacKinnon <bruce@MacBook-Pro.local>
2022-03-31 12:35:27 -0400

    Unit tests

--- tests/unit-test-1.cpp
+++ tests/unit-test-1.cpp
@@ -11,10 +11,26 @@ int main(int argc, const char** argv) {
 
     assert(buf.isEmpty());
 
-    uint16_t oobInt;
-    uint8_t text[8] = { "TEST   " };
-    buf.push((const uint8_t*)&oobInt, text, 8);
-    assert(!buf.isEmpty());
+    // Put some data on the queue
+    {
+        uint16_t oobInt = 18;
+        uint8_t text[8] = { "TEST   " };
+        buf.push((const uint8_t*)&oobInt, text, 8);
+        assert(!buf.isEmpty());
+    }
+
+    // Pop it off
+    {
+        uint16_t oobInt = 0;
+        uint8_t text[8];
+        unsigned int textLen = 8;
+        buf.pop((uint8_t*)&oobInt, text, &textLen);
+        assert(buf.isEmpty());
+        assert(textLen == 8);
+        assert(oobInt == 18);
+    }
+
+
 
 
     return 0;
