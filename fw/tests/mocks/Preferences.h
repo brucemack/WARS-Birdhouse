1cad1c2a31ce9707aa01910d82e8dfbfe30b762a
Bruce MacKinnon <bruce@MacBook-Pro.local>
2022-04-14 18:39:49 -0400

    Added a timeout for TX

---
+++ fw/tests/mocks/Preferences.h
@@ -0,0 +1,15 @@
+#ifndef _Preferences_h
+#define _Preferences_h
+
+#include <stdint.h>
+
+class Preferences {
+public:
+
+    void begin(const char*);
+
+    void getBytes(const char*, void*, unsigned int len);
+    void putBytes(const char*, const void*, unsigned int len);
+};
+
+#endif
